6d77677a0e007645840d6b3753d9a3e9c60e9750
Jhonattan Benitez Ballesteros <40676789+jhonattanbenitez@users.noreply.github.com>
2025-05-09 09:49:39 -0500

    add auto ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,7 +57,15 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
-        <meta name="google-adsense-account" content="ca-pub-8154105300412752"></meta>
+        <meta
+          name="google-adsense-account"
+          content="ca-pub-8154105300412752"
+        ></meta>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8154105300412752"
+          crossOrigin="anonymous"
+        ></script>
       </head>
       <body>
         <header className="border-b py-4 px-6 bg-white shadow-sm">
